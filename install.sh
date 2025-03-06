7a31835a20424d7166d17ff8a5ffe278ffb25905
Matt Bowers <m.a.bowers@stonetable.org.uk>
2025-03-06 20:36:39 +0000

    Set-up proof-of-concept plugin.

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp main.js /Users/matthew/Documents/Vaults/Testbed/.obsidian/plugins/poc-plugin/
+cp manifest.json /Users/matthew/Documents/Vaults/Testbed/.obsidian/plugins/poc-plugin/
+cp styles.css /Users/matthew/Documents/Vaults/Testbed/.obsidian/plugins/poc-plugin/
+cp .hotreload /Users/matthew/Documents/Vaults/Testbed/.obsidian/plugins/poc-plugin/
--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface PocPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: PocPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PocPlugin extends Plugin {
+	settings: PocPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: PocPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PocPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
